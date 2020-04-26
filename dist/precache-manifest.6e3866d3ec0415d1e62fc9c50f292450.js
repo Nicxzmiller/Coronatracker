013d671eb3d95cc5ba23e76b8f1cc2ada687e331
Nicholas <nicxzmiller2@gmail.com>
2020-04-26 09:00:52 +0200

    fixes and deployment

---
+++ dist/precache-manifest.6e3866d3ec0415d1e62fc9c50f292450.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c84eee278ef2d36b8d20efc418babc8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be697f1ecabbf48964cc",
+    "url": "/static/css/main.8a0605e8.chunk.css"
+  },
+  {
+    "revision": "0a9e53bb2b9c63325f15",
+    "url": "/static/js/2.df733f35.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.df733f35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be697f1ecabbf48964cc",
+    "url": "/static/js/main.22a2930a.chunk.js"
+  },
+  {
+    "revision": "c6956f08dfbd9eef8b53",
+    "url": "/static/js/runtime-main.4d4af817.js"
+  }
+]);
